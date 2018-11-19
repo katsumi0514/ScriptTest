e5e6fd70cff2370719a50a7584af4baaa92ff64e
伊井克海 <mt4.05140901@gamil.com>
2018-11-19 17:46:25 +0900

    遅れてすいません。再提出です。

--- Assets/kadai1.cs
+++ Assets/kadai1.cs
@@ -7,43 +7,29 @@ public class kadai1 : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
 		int[] array = { 4, 3, 6, 2, 1 };
-		int change1;
-		int change2;
-		int i;
-		int t;
-		int x;
-		int y;
 
 
-		for (i = 0; i < array.Length - 1; i++) {
-			for (t = i + 1; t < array.Length; t++) {
-				if (array [t] < array [i]) {
-					change1 = array [t];
-					array [t] = array [i];
-					array [i] = change1;
-				}			
 
-			}
-		}
+
 		for (int a = 0; a < array.Length; a++) {
 			Debug.Log (array [a]);
+
 		}
 
+		for (int b = 0; b < array.Length / 2; b++) {
+			int t = array [b];
+			array [b] = array [array.Length - b - 1];
+			array [array.Length - b - 1] = t;
+		}
+		for (int c = 0; c < array.Length; c++) {
+			Debug.Log (array [c]);
+		}
+		
+			
+			
+	}		
+			
 
-			for (x = 0; x < array.Length - 1; x++) {
-				for (y = x + 1; y < array.Length; y++) {
-					if (array [y] > array [x]) {
-						change2 = array [y];
-						array [y] = array [x];
-						array [x] = change2;
-					}
-				}
-			}
-			for (int b = 0; b < array.Length; b++) {
-				Debug.Log (array [b]);
-			}
-
-	}
 							
 			
 	
@@ -53,7 +39,7 @@ public class kadai1 : MonoBehaviour {
 		
 		
 	
-	
+
 	// Update is called once per frame
 	void Update () {
 		
